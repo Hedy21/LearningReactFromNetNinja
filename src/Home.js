9c8620cb5a095147a1a493b0d9ab39874b7de79c
PyronSu <tspaing699@gmail.com>
2024-01-16 22:43:57 +0630

    using state

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+// flutter မှာဆို stateful ဖြစ်အောင်လုပ်သလိုပဲ
+
 const Home = () => {
-    
+
+    const [name, setName] = useState('mario'); //useState ထဲ ဘာ data ဖြစ်ဖြစ်ရ
+    const [age, setAge] = useState(25);
+    //let name = "Thae Su Paing";
+
     const handleClick = (e)=>{
-        console.log('hello ninjas',e);
+        setName('luigi');
+        setAge(21);
+        // console.log('hello ninjas',e);
+        // name = "Saw Su Shin";
     }
 
     const handleClick2 = (name,e)=>{
@@ -9,15 +19,18 @@ const Home = () => {
         console.log(e.target);
     }
 
-    return ( 
+    return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name } is { age } years old.</p>
+            {/* handleClick() q=ဆိုပီး parenthesis နဲ့ရေးလိုက်ရင် click မှမဟုတ်ပဲ page load တာနဲ့ အလုပ်လုပ် */}
             <button onClick={handleClick}>Click Me</button>
+            {/* parameter ကနေ data ကို pass ချင်တဲ့ခါ */}
             <button onClick={(e)=>{
                 handleClick2('Mario',e)
             }}>Click me Again</button>
         </div>
      );
 }
- 
+
 export default Home;
