94fd1823a58e9b42003480097f077924c9ea4210
PyronSu <tspaing699@gmail.com>
2024-01-16 22:29:09 +0630

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+    
+    const handleClick = (e)=>{
+        console.log('hello ninjas',e);
+    }
+
+    const handleClick2 = (name,e)=>{
+        console.log('hello '+name);
+        console.log(e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=>{
+                handleClick2('Mario',e)
+            }}>Click me Again</button>
         </div>
      );
 }
